41339abb6290865e313d620e19182014b2df817f
Ezgi <ezgidiriboun@gmail.com>
2017-02-26 13:41:33 +0300

    Django Girls uygulamam, ilk commit

---
+++ ilk.py
@@ -0,0 +1,27 @@
+print('bu')
+if 3>2:
+	print('çalıştı')
+volume=57
+if volume<20:
+	print('kısık')
+elif 20<= volume <60:
+	print('orta')
+else:
+	print('iyi')
+
+def selam():
+	print('merhaba')
+	print('naber?')
+selam()
+def hi(name):
+	if name=='Ezgi':
+		print('selam ezgi')
+	elif name=='Zeynep':
+		print('selam zeynep')
+	else:
+		print('selam yabancı')
+hi('ali')
+def hi (isim):
+	print('selam'+ isim)
+hi("seda")
+
